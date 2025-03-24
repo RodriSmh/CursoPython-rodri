25d815f68a1929b5ee84356ed32d881808171eb5
Rodri <L20170080@culiacan.tecnm.mx>
2025-03-23 22:36:14 -0700

    3er commit se agrego metodos de cadenas

--- Metodos/metodos_cadenas.py
+++ Metodos/metodos_cadenas.py
@@ -18,11 +18,18 @@ LEN - CUENTA LOS CARACTERES
 ENDSWITH - VERIFICA SI UNA CADENA COMIENZA CON
 STARTSWTH - VERIFICA SI UNA CADENA TERMINA CON
 
-
 REPLACE - REPLAZA UN VALOR POR OTRO
-SPLIT - SEPARA POR UN PARAMETRO DADO
+SPLIT - SEPARA POR UN PARAMETRO DADO si se coloca como parametro un espacion en blanco, regresa un arreglo de las palabras separadas por espacios
 '''
 
 ###
 cadena1= "Ejemplo de cadena"
 cadena2= " Esta es una extencion de la segunda cadena"
+cadena3= "Alfanumerico"
+# print(cadena1.lower())
+# print(cadena1.find("plo"))
+# print(cadena3.isalpha())]
+# print(cadena3.count("a"))
+# print(len(cadena2))
+# print(cadena2.replace("Esta es","Es" ))
+print(cadena1.split(" "))
